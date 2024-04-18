e38d09336cd4c1f6e2450290b7c5aa22d3870e15
Máté <gerendymate@gmail.com>
2024-04-18 19:06:04 +0200

    Alap powerupok

--- src/elte/szofttech/bomberman/model/Player.java
+++ src/elte/szofttech/bomberman/model/Player.java
@@ -1,11 +1,18 @@
 package elte.szofttech.bomberman.model;
 
 import elte.szofttech.bomberman.model.fields.Bomb;
+import elte.szofttech.bomberman.model.fields.Floor;
 import elte.szofttech.bomberman.model.monsters.Monster;
+import elte.szofttech.bomberman.model.powerups.BombDetonator;
+import elte.szofttech.bomberman.model.powerups.BombRangeBonus;
+import elte.szofttech.bomberman.model.powerups.BonusBomb;
+import elte.szofttech.bomberman.model.powerups.PowerUp;
 
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player extends Entity {
     //Control key codes
@@ -19,6 +26,8 @@ public class Player extends Entity {
     private int bombCapacity;
     private int placedBombs;
     public boolean isAlive;
+    public boolean hasDetonator;
+    public List<Bomb> bombsOnGround;;
 
     public Player(int x, int y, int up, int down, int left, int right, int bombButton, GameEngine engine) {
         super(x, y);
@@ -32,14 +41,20 @@ public class Player extends Entity {
         this.bombRadius = 3;
         this.placedBombs = 0;
         this.isAlive = true;
+        this.bombsOnGround = new ArrayList<>();
+        this.hasDetonator = false;
         engine.getBoard()[this.y][this.x].setWalkable(false);
     }
 
     public int getX(){return x;}
     public int getY(){return y;}
+    public int getBombRadius(){return bombRadius;}
     public int getbombCapacity(){return bombCapacity;}
     public int getPlacedBombs(){return placedBombs;}
+    public List<Bomb> getBombsOnGround() {return bombsOnGround;}
     public void setPlacedBombs(int n){this.placedBombs = n;}
+    public void setBombRadius(int n) {this.bombRadius = n;}
+    public void setBombCapacity(int n){this.bombCapacity = n;}
 
     public void move(KeyEvent key){
       if (!this.isAlive) {return;}
@@ -67,12 +82,32 @@ public class Player extends Entity {
           }
       }
       else if (keyAsInt == this.bombButton){
-          if(engine.getBoard()[this.y][this.x].canPlaceBomb()){
-              engine.detonateBomb(new Bomb(currentX*engine.getTILE_SIZE(), currentY*engine.getTILE_SIZE(), this.bombRadius, 3, engine.getTILE_SIZE()),this);
+          if(engine.getBoard()[this.y][this.x].canPlaceBomb() && this.hasDetonator == false){
+            Bomb newBomb = new Bomb(currentX * engine.getTILE_SIZE(), currentY * engine.getTILE_SIZE(), this.bombRadius, 3, engine.getTILE_SIZE());
+            this.bombsOnGround.add(newBomb);
+            System.out.println("Bombák:" + bombsOnGround.toString());
+            engine.detonateBomb(newBomb, this);
           }
+          /*else if (engine.getBoard()[this.y][this.x].canPlaceBomb() && this.hasDetonator == true){
+            if (this.bombsOnGround.size() < this.bombCapacity){
+                Bomb newBomb = new Bomb(currentX * engine.getTILE_SIZE(), currentY * engine.getTILE_SIZE(), this.bombRadius, 3, engine.getTILE_SIZE());
+                this.bombsOnGround.add(newBomb);
+                System.out.println("Bombák:" + bombsOnGround.toString());
+                engine.detonateBomb(newBomb, this);
+            }
+            else if(this.bombsOnGround.size() >= this.bombCapacity)
+            for (Bomb bomb : bombsOnGround) {
+                engine.detonateBombImmediately(bomb, this);
+            }
+          }*/
       }
       if((currentX != this.x || currentY != this.y) && !(engine.getBoard()[currentY][currentX] instanceof Bomb) ) engine.getBoard()[currentY][currentX].setWalkable(true);
       engine.getBoard()[this.y][this.x].setWalkable(false);
+      if (engine.getBoard()[currentY][currentX] instanceof PowerUp) {
+        PowerUp currentPowerUp = (PowerUp) engine.getBoard()[currentY][currentX];
+        powerUpPickup(currentPowerUp);
+        engine.getBoard()[currentY][currentX] = new Floor(currentX * engine.getTILE_SIZE(), currentY * engine.getTILE_SIZE(), engine.getTILE_SIZE());
+    }
   }
 
   public void die(){this.isAlive = false;}
@@ -88,6 +123,21 @@ public class Player extends Entity {
             this.isAlive = false;
         }
     }
+    
+    public void powerUpPickup(PowerUp e) {
+            if (e instanceof BombRangeBonus) {
+                ((BombRangeBonus) e).applyOnPlayer(this);
+                System.out.println("range updated" + bombRadius);
+            } else if (e instanceof BonusBomb) {
+                ((BonusBomb) e).applyOnPlayer(this);
+                System.out.println("counter updated" + bombCapacity);
+            }else if (e instanceof BombDetonator) {
+                ((BombDetonator) e).applyOnPlayer(this);
+                System.out.println("Detonator collected!");
+            }
+        }
+
+
     public void draw(Graphics g) {
       int ts = engine.getTILE_SIZE();
       g.setColor(Color.RED);
