5b84dc1f9b231cb89b11ac7b93a53739a66d04ad
Laczkovics Botond <botond.laczkovics@gmail.com>
2024-04-26 00:13:25 +0200

    refactor

--- src/elte/szofttech/bomberman/tests/MonsterTest.java
+++ src/elte/szofttech/bomberman/tests/MonsterTest.java
@@ -1,4 +1,33 @@
 package elte.szofttech.bomberman.tests;
 
+import elte.szofttech.bomberman.model.GameEngine;
+import elte.szofttech.bomberman.model.monsters.BasicMonster;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MonsterTest {
+
+    BasicMonster monster;
+    GameEngine engine;
+    @BeforeEach
+    public void setUp(){
+        engine = new GameEngine(1000,1000);
+        monster = new BasicMonster(1,3,engine,1);
+    }
+
+    @Test
+    public void testConstructor(){
+        assertEquals(1,monster.getX());
+        assertEquals(3,monster.getY());
+    }
+
+    @Test
+    public void canMoveToValidField(){
+        assertEquals(1, monster.getX());
+       // monster.move();
+    }
+
 }
